bfd9bd6f563117d518b9e14ed54d39240e4613e5
KriptoBaba382 <199w29c@mail.ru>
2025-06-18 16:26:47 +0300

    Add querySelectorAll

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const modalButton = document.querySelector ('.button-record')
 function openModal () {
     modalOpen.classList.remove ('hiden')
 }
-const modalButton2 = document.querySelector ('.button-recording')
-const modalButton3 = document.querySelector ('.button.form')
+const modalButton2 = document.querySelectorAll  ('.button-recording')
+const modalButton3 = document.querySelector('.button.form')
 const modalOpen = document.querySelector ('.modal-bg')
 modalButton.addEventListener('click', openModal )
-modalButton2.addEventListener ('click', openModal)
 modalButton3.addEventListener ('click', openModal)
 const modalClose = document.querySelector ('.modal-close')
 function closeModal () {
     modalOpen.classList.add ('hiden')
 }
-modalClose.addEventListener ('click', closeModal     )
+modalClose.addEventListener ('click', closeModal)
+for (let i = 0; i < 6; i++) {
+    modalButton2[i].addEventListener('click', openModal)
+}
